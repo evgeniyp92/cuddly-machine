68e9cd206f13945580dc37da35f4c26803e1bf3c
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2023-01-05 20:07:50 +0100

    set up queryClient, provider, and basic query

--- 1-blogem-ipsum/src/App.jsx
+++ 1-blogem-ipsum/src/App.jsx
@@ -1,14 +1,19 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { Posts } from './Posts';
+import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const qClient = new QueryClient();
 
 function App() {
-  return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
-  );
+    return (
+        // provide React Query client to App
+        <QueryClientProvider client={qClient}>
+            <div className='App'>
+                <h1>Blog Posts</h1>
+                <Posts />
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
